eae62650ae9151b1a3da64f27e410033f07e17d0
@JaviertINC <luis.cortes.jaar@gmail.com>
2020-03-20 11:27:02 -0300

    Update rgb2hex.php

--- functions/rgb2hex.php
+++ functions/rgb2hex.php
@@ -1,1 +1,7 @@
+//USO
+// rgb_hex(76, 175, 80); //return "#4caf50"
 
+function rgb_hex($rgb){
+	$rgbarr = explode(",",$rgb,3);
+	echo sprintf("#%02x%02x%02x", $rgbarr[0], $rgbarr[1], $rgbarr[2]);
+}
