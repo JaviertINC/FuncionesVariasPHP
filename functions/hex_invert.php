b74c35941963ba7ace51fad921f46fa4f1e5291f
@JaviertINC <luis.cortes.jaar@gmail.com>
2020-03-23 19:55:16 -0300

    Create hex_invert.php

---
+++ functions/hex_invert.php
@@ -0,0 +1,14 @@
+//Uso
+//  hex_invert("#4caf50"); //return "#af4cab"
+
+function hex_invert($color){
+    $color = str_replace('#', '', $color);
+    if (strlen($color) != 6){ return '000000'; }
+    $rgb = '';
+    for ($x=0;$x<3;$x++){
+        $c = 255 - hexdec(substr($color,(2*$x),2));
+        $c = ($c < 0) ? 0 : dechex($c);
+        $rgb .= (strlen($c) < 2) ? '0'.$c : $c;
+    }
+    return '#'.$rgb;
+}
